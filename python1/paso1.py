dede34727a49a6a0ec899dfb6242f4986f7abada
LichtEwig <lichtewig73@gmail.com>
2023-10-10 20:45:27 -0300

    entorno1

--- python1/paso1.py
+++ python1/paso1.py
@@ -2,4 +2,6 @@ print('hola mundo')
 numero1=5
 numero2=21
 resultado= numero1 + numero2
-type(numero1)
+print('el tipo de dato del primer numero es: ',type(numero1))
+print('el resultado de la suma es: ',resultado)
+print('el resultado de la suma es: '+str(resultado))
